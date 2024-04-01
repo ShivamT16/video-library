a1ea8255f193c5bc0b2b1257785ec42d74f5a96a
Shivam Tripathi <109875608+ShivamT16@users.noreply.github.com>
2024-04-01 14:24:39 +0530

    feat: Add Notes feature on single video page

--- src/Pages/VideoPage.js
+++ src/Pages/VideoPage.js
@@ -1,12 +1,34 @@
 import "./videopage.css"
-import { useContext } from "react"
+import { v4 as uuid } from 'uuid'
+import { useContext, useState } from "react"
 import { WatchLaterContext,VideoContext } from ".."
 import PlaylistAddRoundedIcon from '@mui/icons-material/PlaylistAddRounded';
 import EditNoteRoundedIcon from '@mui/icons-material/EditNoteRounded';
 
 export const VideoPage = ({videos}) => {
+    const [input, setInput] = useState(false)
+    const [addNotes, setAddNotes] = useState([])
+    const [notes, setNotes] = useState([])
+    
     const {videoId} = useContext(VideoContext)
     const {watchLaterList,handleWatchLater,handleDelete,AccessTimeRoundedIcon,AccessTimeFilledRoundedIcon} = useContext(WatchLaterContext)
+
+    const handleNotes = (e) => {
+       e.preventDefault();
+       const newNotes = {note: addNotes, id:uuid() }
+       setNotes([...notes,newNotes])
+       setAddNotes("")
+    }
+
+    const handleNoteDelete = (id) => {
+        setNotes(notes.filter((item) => item.id !== id))
+    }
+
+    const handleUpdate = (id) => {
+        const find = notes.find((item) => item.id === id)
+        setAddNotes(find.note)
+        handleNoteDelete(id)
+    }
 
     return(
         <div>
@@ -28,9 +50,30 @@ export const VideoPage = ({videos}) => {
     <p onClick={() => handleWatchLater(item) } > <AccessTimeRoundedIcon ></AccessTimeRoundedIcon></p>
     }
     <PlaylistAddRoundedIcon sx={{ fontSize: 30 }} />
-    <EditNoteRoundedIcon sx={{ fontSize: 30 }} />
+    <EditNoteRoundedIcon onClick={() => setInput(!input) } sx={{ fontSize: 30 }} />
     </div>
      </div>
+     {input &&
+     <form>
+        <div className="notes">
+        <label htmlFor="Notes" ></label>
+        <input className="notes-input" type="text" placeholder="Add Note..." value={addNotes} onChange={(e) => setAddNotes(e.target.value) } />
+        <button className="submit-btn" onClick ={handleNotes} >Add Note</button>
+        </div>
+     </form>
+      }
+      <div className="display-notes">
+      { 
+        notes.map((element) => 
+        <div className="Notes" key={element.id}>
+            <h4 >{element.note}</h4>
+            <button className="submit-btn" onClick={() => handleNoteDelete(element.id)}>Delete</button>
+            <button className="submit-btn" onClick={() => handleUpdate(element.id) } >Update</button>
+            
+        </div>
+        )
+      }
+      </div>
     </div>
  )
 } ) }
